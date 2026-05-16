c32af27279c6e9699adf03a10038fe5182d01592
Liz0rdX-binary <khqalied66@gmail.com>
2026-05-16 23:17:36 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Made By KhaledX For Void",
         // Activity type number (0 = Playing).
         type: 0, 
       },
